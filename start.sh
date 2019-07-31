a72169a83ab6c86825dc97aedea0d36715badd44
Osman Mutlu <osmanmutlu92@gmail.com>
2019-07-31 13:40:25 +0300

    Give time for API's to start

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 git pull origin master
+nohup python3 /emw_pipeline_nf/bin/neuroner/neuroner_flask.py 2> /dev/null &
+export PYTHONPATH=/emw_pipeline_nf/bin
 nohup python3 /emw_pipeline_nf/bin/classifier/classifier_flask.py 2> /dev/null &
 nohup python3 /emw_pipeline_nf/bin/sent_classifier/classifier_flask.py 2> /dev/null &
 nohup python3 /emw_pipeline_nf/bin/trigger_classifier/classifier_flask.py 2> /dev/null &
-nohup python3 /emw_pipeline_nf/bin/neuroner/neuroner_flask.py 2> /dev/null &
-export PYTHONPATH=/emw_pipeline_nf/bin
+sleep 60
 nextflow run emw_pipeline.nf
