ea8a17fdb9ebad80bb138531a7f1781ac96433f4
alabrashjr <alabrash.abd@gmail.com>
2019-08-19 13:32:53 +0300

    adding violent classifier command to start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@ export PYTHONPATH=/emw_pipeline_nf/bin
 nohup python3 /emw_pipeline_nf/bin/classifier/classifier_flask.py 2> /dev/null &
 nohup python3 /emw_pipeline_nf/bin/sent_classifier/classifier_flask.py 2> /dev/null &
 nohup python3 /emw_pipeline_nf/bin/token_classifier/classifier_flask.py 2> /dev/null &
+nohup python3 /emw_pipeline_nf/bin/violent_classifier/classifier_flask.py 2> /dev/null &
+
 sleep 60
 nextflow run emw_pipeline.nf
