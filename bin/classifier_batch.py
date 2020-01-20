81b551542de6a1a09ccb03d1e20771156488bb81
alabrashjr <alabrash.abd@gmail.com>
2020-01-20 16:50:08 +0300

    update classifier_batch class

--- bin/classifier_batch.py
+++ bin/classifier_batch.py
@@ -1,3 +1,10 @@
+#Abdurrahman Beyaz. abdurrahmanbeyaza@gmail.com
+"""
+This class is one of component of the pipeline.It reads a given json files as a batch and send request to FLASK documenet classifier API.
+This component may be positioned either as first where the news articles are already in JSON format, or as second after the news articles that are in formatted stuctured like HTML converted to JSON format.
+the lines that end with '#' must be used in case of this component is used as first and lines that end with '##' must be used in case of this component is used as second. 
+"""
+
 import argparse
 import json
 import requests
@@ -6,7 +13,7 @@ from utils import write_to_json
 from utils import dump_to_json
 from utils import read_from_json
 from utils import change_extension
-from datetime import datetime
+from datetime import datetime # can be not used check it 
 
 def get_args():
     '''
@@ -26,26 +33,19 @@ def request(texts):
 
 if __name__ == "__main__":
     args = get_args()
-    # jsons = eval(re.sub(r"\[QUOTE\]", r"'", args.data))
-    files=args.input_files.strip("[ ]").split(",") # when doc classifier component is first
-    #files = eval(args.input_files) #when doc classifier component its not the first 
+    #files=args.input_files.strip("[ ]").split(",") # when doc classifier component is first
+    files = eval(args.input_files) #when doc classifier component its not the first 
     jsons = []
     for filename in files:
         filename=filename.strip("' ")
-        #read_file=read_from_json(args.out_dir+filename) ##
-        #if read_file=="":continue ##
-        #jsons.append(read_file) ## when doc classifier component its not first
-        jsons.append(read_from_json(filename)) #when doc classifier component is first
+        read_file=read_from_json(args.out_dir+filename) ##
+        if read_file=="":continue ##
+        jsons.append(read_file) ## when doc classifier component its not first
+        # jsons.append(read_from_json(filename)) #when doc classifier component is first
     rtext = request([data["text"] for data in jsons])
     event_sentences = rtext["event_sentences"]
     output_data = []
     for i,data in enumerate(jsons):
-######## those are only for 20_K_Sample expirement 
-        if "token_labels" in data:
-                del data["token_labels"]
-        if "tokens" in data:
-                del data["tokens"]
-########
         out = int(rtext["outputs"][i])
         data["doc_label"] = out
         data["length"] = len(data["text"])
