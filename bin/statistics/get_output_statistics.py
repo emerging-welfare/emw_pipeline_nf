057e64918a4a02f473a2f1c7f4954109f1d59a3d
Alaeddin <alaeddinselcukgurel@gmail.com>
2020-06-04 21:12:43 +0300

    Pipeline to json table

--- bin/statistics/get_output_statistics.py
+++ bin/statistics/get_output_statistics.py
@@ -1,6 +1,24 @@
 import glob
 import json
 from collections import Counter
+import csv
+import json
+
+
+# videos.py
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Text')
+parser.add_argument('input', type=str, help='Input files should be nextflow output')
+parser.add_argument('tag', type=str, help='Tag')
+parser.add_argument('type', type=str, help='file type')
+
+
+args = parser.parse_args()
+input_folder = args.input
+tag = args.tag
+file_type = args.type
 
 def postprocess(data):
     all_tokens = []
@@ -38,18 +56,57 @@ count = 0
 empty_list_flair_output = 0
 protest_but_no_place = 0
 protest_but_no_place_list = []
-for elem in glob.glob("new_india/19052020_10k/*.json"):
+
+
+"""
+--input_file
+--output_file
+--place name, other features
+--argparse kullanarak
+--help script hakkında bilgi verecek
+json dosyasını pipeline outputbatch klasörüne koyabiliriz
+bütün protestolarda ya da place name olan dosyalarda trigger var mı?
+place name trigger olan cümlede mi? percentage, aynısı flair output için de aynısı
+flair output and place name extractor comparison
+logging --  exception file --
+--tag place, trigger, time, 
+nextflowoutputtohumanreadable
+nf2
+
+"""
+
+"""
+new_india/19052020_10k/*.json
+"""
+for elem in glob.glob(input_folder+"*json"):
     with open(elem) as f:
         content = json.loads(f.read())
         if content['doc_label'] == 1:
             count = count + 1
-            print(count)
+            #print(count)
             b_place_count = 0
             for idx, token_label in enumerate(content['token_labels']):
-                if token_label == "B-place":
+                if token_label == "B-"+tag:
+                    token_items = []
+                    token_items.append(content['tokens'][idx])
+                    flag = True
+                    for second_index, elem in enumerate(content['token_labels'][idx+1:]):
+                        #print(elem)
+
+                        if elem == "I-"+tag:
+                            print('YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS')
+                            print(content['id'])
+                            token_items.append(content['tokens'][idx+second_index+1])
+                        else:
+                            break
+                    print(" ".join(token_items))
                     b_place_count = b_place_count + 1
-                    place_name_list.append(content['tokens'][idx])
-                    print(token_label)
+                    """
+                    if content['tokens'][idx] == "South":
+                        print(content['id'])
+                    """
+                    place_name_list.append(" ".join(token_items))
+                    #print(token_label)
                     #print(content['tokens'][idx])
             if b_place_count == 0:
                 protest_but_no_place = protest_but_no_place + 1
@@ -64,12 +121,11 @@ for elem in glob.glob("new_india/19052020_10k/*.json"):
                 print("There is no flair output")
                 protest_flair_count = protest_flair_count + 1
 
-
 # STATISTICS
 print("Total protest number is " + str(count))
 print("Total protest number without flair_output " + str(protest_flair_count))
-print("Most Frequent Words : ")
-print(Counter(place_name_list).most_common(10))
+print("Most Frequent Place Names : ")
+print(Counter(place_name_list).most_common(80))
 print("Protest without flair output total protest file ratio " + str(protest_flair_count/count))
 
 
@@ -80,3 +136,33 @@ print("Protest with flair output total protest file ratio " + str(1-(protest_fla
 print("Protest but no place " + str(protest_but_no_place))
 print("Protest but no place protest ratio" + str(protest_but_no_place/count))
 print(protest_but_no_place_list)
+
+
+
+
+
+"""
+Her document için id 
+doc sentences [[B-place, I-place], []]
+"""
+
+
+file_dict = {
+    "total_document" : count,
+    "protest_without_flair": protest_flair_count,
+    "most_frequent_place_names": Counter(place_name_list).most_common(80),
+    "protest_without_flair_total_protest_ratio": protest_flair_count/count,
+    "Protest_but_no_place": protest_but_no_place,
+    "Protest_but_no_lace_protest_ratio": protest_but_no_place/count
+    }
+
+
+
+if file_type == "csv":
+    with open('mycsvfile.csv', 'w') as f:  # Just use 'w' mode in 3.x
+        w = csv.DictWriter(f, file_dict.keys())
+        w.writeheader()
+        w.writerow(file_dict)
+else:
+    with open('mycsvfile.json', 'w') as fp:
+        json.dump(file_dict, fp)
---
+++ bin/statistics/pipeline_to_json.py
@@ -0,0 +1,151 @@
+import glob
+import json
+from collections import Counter
+import csv
+import json
+
+def postprocess(data):
+    all_tokens = []
+    # sent_count = 0
+    all_token_labels = []
+
+    for i, token in enumerate(data["tokens"]):
+        if token == "SAMPLE_START":
+            token_labels = []
+            tokens = []
+        elif token == "[SEP]":
+            all_tokens.append(tokens)
+            # if data["sent_labels"][sent_count] == 0: # If sentence's label is 0, ignore all predicted tokens and reset them to 'O' tag.
+            #     token_labels = ["O"] * len(token_labels)
+
+            all_token_labels.append(token_labels)
+            # sent_count += 1
+            token_labels = []
+            tokens = []
+        else:
+            tokens.append(token)
+            token_labels.append(data["token_labels"][i])
+
+    all_token_labels.append(token_labels)
+    all_tokens.append(tokens)
+    data["token_labels"] = all_token_labels
+    data["tokens"] = all_tokens
+    return data
+
+"""
+protest_flair_count = 0
+place_name_list = []
+flair_name_list = []
+count = 0
+empty_list_flair_output = 0
+protest_but_no_place = 0
+protest_but_no_place_list = []
+"""
+
+
+"""
+Dictionary başta eklemek lazım
+öncesindekilerde eklenmemiş olacak
+"""
+
+json_list = []
+token_list_all = []
+for elem in glob.glob("new_india/19052020_10k/*.json"):
+    print(elem)
+    with open(elem) as f:
+        content = json.loads(f.read())
+
+    for elem in content['token_labels']:
+        if elem.startswith("B-") and elem not in token_list_all:
+            print(elem)
+            token_list_all.append(elem)
+print(token_list_all)
+
+
+
+
+
+
+
+
+json_file = open("pipeline_to_json.json", "w+")
+for elem in glob.glob("new_india/19052020_10k/*.json"):
+    with open(elem) as f:
+        content = json.loads(f.read())
+
+
+
+        content = postprocess(content)
+
+    doc_json = {}
+    doc_json['document'] = {}
+    doc_json['document']['sentences'] = []
+    doc_json['document']['id'] = content['id']
+    doc_json['document']['url'] = ""
+    doc_json['document']['title'] = content['title']
+    doc_json['document']['is_violent'] = content['is_violent']
+
+
+
+
+
+    #print(content)
+    for sent_idx,sentence_tokens in enumerate(content['token_labels']):
+        json_sentence = {}
+        json_sentence['sentence'] = content['sentences'][sent_idx]
+        for token_feature in token_list_all:
+            json_sentence[token_feature] = []
+            for idx, token in enumerate(sentence_tokens):
+                if token.startswith(token_feature):
+                    token_items = []
+                    token_items.append(content['tokens'][sent_idx][idx])
+                    for second_index, elem in enumerate(content['token_labels'][sent_idx][idx+1:]):
+                        #print(elem)
+
+                        if elem.startswith("I-"+token_feature[1:]):
+                            #print('YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS')
+                            #print(content['id'])
+                            token_items.append(content['tokens'][sent_idx][idx+second_index+1])
+                        else:
+                            break
+                        
+                    print(token_items)
+                    full_item = " ".join(token_items)
+                    print(full_item)
+                    json_sentence[token_feature].append(full_item)
+                    #print(json_sentence)
+        #print(json_sentence)
+        doc_json['document']['sentences'].append(json_sentence)
+        print(doc_json)
+        #print(doc_json)
+    
+                    
+    json_file.write(json.dumps(doc_json)+"\n")
+
+            #print(content['sentences'][idx])
+
+
+
+"""
+    {
+        document : {
+            document_id: "",
+            document_url: "",
+            title: "",
+            is_violent : 1,
+            sentences : [
+                {sentence_idx : 2, partipant_semantic, organizer_semantic, sentence_label, target_semantic, trigger_semantic<},
+                {}
+            ],
+
+        }
+    }
+
+    
+    
+    tüm   featureları kontrol et yoksa json list append
+    bunun output'undan istatistik
+    json dosyaya yazdır
+    dosyaya tarih ekle
+
+"""
